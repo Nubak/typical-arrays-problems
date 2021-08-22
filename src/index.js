420fec4610c6624308fdb9bae171c52b131509e1
Nubak <verynubgosu@gmail.com>
2021-08-22 10:13:33 +0300

    feat: solution for task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 
 exports.min = function min (array) {
-  return 0;
+  
+  if (array && array.length) 
+  {
+    let min = Math.min.apply(null, array);
+    return min;
+  
+} 
+else {
+
+return 0;
+
+}
 }
 
 exports.max = function max (array) {
-  return 0;
+
+if (array && array.length)
+
+{
+  let max = Math.max.apply(null, array);
+  return max;
+  
+} 
+else 
+{
+return 0;
+
+}
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array && array.length)
+   {
+     return array.reduce((a, b) => (a + b)) / array.length;
+  
+}
+else {
+return 0;
+}
+
 }
